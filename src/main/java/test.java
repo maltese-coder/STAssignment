62a8f8ab0dcd269b72df66f91599fe579013af01
Matthew Xuereb <matxuereb@hotmail.co.uk>
2016-11-19 14:00:55 +0100

    Test Commit 2

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -6,6 +6,8 @@ public class test
     public static void main(String args[])
     {
         System.out.println("Hello World");
-
+        /*This is the second commit
+        what happens now?
+         */
     }
 }
