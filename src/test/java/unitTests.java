bd181c8aaab0e949b934e75d67e49db91ad11b75
mbonnici94 <mbonnici94@gmail.com>
2016-11-29 10:21:28 +0100

    Creation of test class

---
+++ src/test/java/unitTests.java
@@ -0,0 +1,16 @@
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by matt on 21/11/2016.
+ */
+public class unitTests {
+
+
+    @Before
+    public void setup()
+    {
+
+    }
+    
+}
