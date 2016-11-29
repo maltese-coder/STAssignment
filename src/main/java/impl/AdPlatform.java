de5df9316d6f0852d3da02307b9273475a7f6f78
mbonnici94 <mbonnici94@gmail.com>
2016-11-29 12:16:25 +0100

    Added Tests on  - registerAffiliate  - settleAffiliateBalance
     Added getAffiliateCount method on AdPlatform Class

--- src/main/java/impl/AdPlatform.java
+++ src/main/java/impl/AdPlatform.java
@@ -22,15 +22,18 @@ public class AdPlatform implements iAdProvider
     {
         affiliatesMap.put(affiliate.getId(),affiliate);
         return true;
-
     }
 
-
-    public boolean settleAffiliateBalance(Affiliate affiliate)
+    public int getAffiliateCount()
     {
-        if (!(affiliate.getBalance() < 5))
+        return  affiliatesMap.size();
+    }
+
+    public double settleAffiliateBalance(Affiliate affiliate)
+    {
+        if ((affiliate.getBalance() < 5))
         {
-            return false;
+            return -1;
         }
 
         double commission = getCommission(affiliate);
@@ -38,7 +41,7 @@ public class AdPlatform implements iAdProvider
 
         affiliate.setBalance(0);
 
-        return true;
+        return payment;
     }
 
     private double getCommission(Affiliate affiliate)
