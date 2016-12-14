d4e2bf9c1f3531b52715be65dc50074d8793e971
mbonnici94 <mbonnici94@gmail.com>
2016-12-14 09:45:44 +0100

    Added webTests and WebAppObject

---
+++ src/test/java/pageobjects/WebAppObject.java
@@ -0,0 +1,56 @@
+package pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+/**
+ * Created by matt on 13/12/2016.
+ */
+public class WebAppObject {
+
+    WebDriver driver;
+
+    public WebAppObject(WebDriver _driver)
+    {
+        driver = _driver;
+    }
+
+    public void visit(){
+        driver.get("localhost:3000");
+    }
+
+    public void login(String username, String password){
+        driver.findElement(By.id("username")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.id("loginBtn")).click();
+    }
+
+    public void correctLogin(){
+        driver.findElement(By.id("username")).sendKeys("Affiliate1");
+        driver.findElement(By.id("password")).sendKeys("Password1");
+        driver.findElement(By.id("loginBtn")).click();
+    }
+
+    public String getElementText(String name)
+    {
+        return driver.findElement(By.cssSelector(name)).getAttribute("innerText");
+    }
+
+    public void clickElement(String name){
+        driver.findElement(By.id(name)).click();
+    }
+
+    public void withdraw() {
+        this.clickElement("withdrawBtn");
+        sleep(1);
+        this.clickElement("withdrawOkBtn");
+        sleep(1);
+    }
+
+    public void sleep(int seconds) {
+        try {
+            Thread.sleep(seconds*1000);
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+    }
+}
---
+++ src/test/java/webTests.java
@@ -0,0 +1,88 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import pageobjects.WebAppObject;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+public class webTests {
+
+    WebDriver       driver;
+    WebAppObject    wao;
+    ChromeOptions   options;
+
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "/users/matt/documents/chromedriver/chromedriver");
+
+        options = new ChromeOptions();
+        options.setExperimentalOption("excludeSwitches", Arrays.asList("--test-type"));
+        driver = new ChromeDriver(options);
+        driver.manage().window().setSize(new Dimension(1366, 768));
+
+        wao = new WebAppObject(driver);
+    }
+
+    @Test
+    public void testCorrectLogin()
+    {
+        wao.visit();
+        wao.login("Affiliate1", "Password1");
+
+        assertEquals("Welcome Affiliate1", wao.getElementText("p.wText"));
+    }
+
+    @Test
+    public void testIncorrectLogin(){
+        wao.visit();
+        wao.login("Affiliate1", "Pass");
+
+        assertEquals("Wrong username or password", wao.getElementText("p.loginStatusText"));
+    }
+
+    @Test
+    public void testCorrectWithdraw(){
+        wao.visit();
+        wao.correctLogin();
+
+        wao.sleep(1);
+
+        int balanceBefore = Integer.parseInt(wao.getElementText("span.balanceText"));
+
+        wao.clickElement("withdrawBtn");
+        wao.sleep(1);
+        wao.clickElement("withdrawOkBtn");
+        wao.sleep(1);
+
+        int balanceAfter = Integer.parseInt(wao.getElementText("span.balanceText"));
+
+        assertEquals(balanceBefore, 10);
+        assertEquals(balanceAfter, 0);
+    }
+
+    @Test
+    public void testIncorrectWithdraw(){
+        wao.visit();
+        wao.correctLogin();
+
+        wao.sleep(1);
+
+        wao.withdraw();
+
+        wao.clickElement("withdrawBtn");
+        wao.sleep(1);
+
+        assertEquals("You do not have enough balance to withdraw.", wao.getElementText("p.errorText"));
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
