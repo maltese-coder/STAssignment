f18b072a12edf9764c0093e296de63f7937a4009
Matthew Xuereb <matxuereb@hotmail.co.uk>
2017-01-05 15:56:30 +0100

    Fixed @Before Hook for web testing

--- src/test/java/cucumberTests/StepDefinitions.java
+++ src/test/java/cucumberTests/StepDefinitions.java
@@ -4,7 +4,7 @@ import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.junit.Before;
+import cucumber.api.java.Before;
 import pageobjects.WebAppObject;
 
 import static org.junit.Assert.assertEquals;
